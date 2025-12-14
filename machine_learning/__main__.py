1e8747534b467a4b752e41ef0e0518960f683d04
k0killette <c.jalouneix@gmail.com>
2025-12-14 19:01:55 +0100

    amélioration de la gestion des erreurs pour l'affichage et l'export des résultats

--- machine_learning/__main__.py
+++ machine_learning/__main__.py
@@ -12,12 +12,16 @@ def main():
         check_data(df)
         # Sélection des features
         X = select_features(df, feature_cols)
-        # Test de sensibilité pour le paramètre contamination
-        sensitivity = contamination_sensitivity_test(X)
+        # # Test de sensibilité pour le paramètre contamination
+        # sensitivity = contamination_sensitivity_test(X)
         # Entraînement du modèle
         model = train_isolation_forest(X, contamination=0.20)
         # Application du modèle
         df_scored = apply_model(df, X, model)
+        # Résumés + anomalies
+        anomalies = summarize_results(df_scored, feature_cols)
+        # Export CSV
+        export_results(df_scored, 'regles_anomalies.csv')
 
     except (FileNotFoundError, RuntimeError, ValueError) as e:
         print(f"Erreur dans le processus : {e}")
@@ -25,11 +29,6 @@ def main():
 
 
 
-    # # Résumés + anomalies
-    # anomalies = summarize_results(df_scored, feature_cols)
-
-    # # Export CSV
-    # export_results(df_scored, "regles_anomalies.csv")
 
     # # Visualisations
     # plot_anomalies(df_scored, anomalies, "anomalies_regles_barchart.png")
