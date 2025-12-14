7d27c31701ce23d43b19cd0a28dbf5ce6798f21b
k0killette <c.jalouneix@gmail.com>
2025-12-14 17:22:52 +0100

    amélioration de la gestion des erreurs pour l'entrainement et l'application du modèle

--- machine_learning/__main__.py
+++ machine_learning/__main__.py
@@ -1,6 +1,6 @@
 from data_loading import load_data
 from data_preprocessing import check_data, select_features, feature_cols
-from model_isolation_forest import train_isolation_forest, apply_model
+from model_isolation_forest import train_isolation_forest, apply_model, contamination_sensitivity_test
 from model_results import summarize_results, export_results
 from visualization import plot_anomalies
 
@@ -12,16 +12,18 @@ def main():
         check_data(df)
         # Sélection des features
         X = select_features(df, feature_cols)
+        # Test de sensibilité pour le paramètre contamination
+        sensitivity = contamination_sensitivity_test(X)
+        # Entraînement du modèle
+        model = train_isolation_forest(X, contamination=0.20)
+        # Application du modèle
+        df_scored = apply_model(df, X, model)
 
     except (FileNotFoundError, RuntimeError, ValueError) as e:
         print(f"Erreur dans le processus : {e}")
         return
 
-    # # Entraînement du modèle
-    # model = train_isolation_forest(X, contamination=0.20)
 
-    # # Application du modèle
-    # df_scored = apply_model(df, X, model)
 
     # # Résumés + anomalies
     # anomalies = summarize_results(df_scored, feature_cols)
