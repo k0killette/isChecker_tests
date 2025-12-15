55f6827c9644b9d6e080341e707e0867c2a1647d
k0killette <c.jalouneix@gmail.com>
2025-12-15 13:24:34 +0100

    amélioration de la gestion des erreurs dans le routage

--- machine_learning/__main__.py
+++ machine_learning/__main__.py
@@ -3,6 +3,7 @@ from data_preprocessing import check_data, select_features, feature_cols
 from model_isolation_forest import train_isolation_forest, apply_model, contamination_sensitivity_test
 from model_results import summarize_results, export_results
 from visualization import plot_scatter_anomalies, plot_barchart_anomalies
+from routing_json import update_json_rules_routing
 
 def main():
     try:
@@ -25,6 +26,8 @@ def main():
         # Visualisations
         plot_scatter_anomalies(df_scored, 'anomalies_scatter.png')
         plot_barchart_anomalies(anomalies, 'anomalies_regles_barchart.png')
+        # Routage construit à partir de l'anomaly_score
+        update_json_rules_routing(df_scored)
 
     except (FileNotFoundError, RuntimeError, ValueError) as e:
         print(f"Erreur dans le processus : {e}")
